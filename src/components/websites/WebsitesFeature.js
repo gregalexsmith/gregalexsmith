66b78debf685b0adbb922d963f6614898078531d
gregalexsmith <gregalexsmith@gmail.com>
2018-09-10 19:36:48 -0400

    add soundscape generator to websites

--- src/components/websites/WebsitesFeature.js
+++ src/components/websites/WebsitesFeature.js
@@ -3,43 +3,7 @@ import './websites-styles.css';
 
 import { Websites_Data } from './websites_data.js';
 
-
-const WebsitesFeature = (props) => {
-
-    const renderSiteFeatures = Websites_Data.websites.map( (site, index) =>
-        <div className="site-container" key={index}>
-          <h2 className="sites-title">{site.title}</h2>
-          <div className="sites-images">
-            <img src={site.images[0]} alt="Media Made Website 1"/>
-            <img src={site.images[1]} alt="Media Made Website 2"/>
-          </div>
-          <div className="sites-info">
-            <section>
-              <h3>Description</h3>
-              <p>{site.desc}</p>
-            </section>
-            <section>
-              <h3>Technologies Used</h3>
-              <p>
-                <b>Front End:</b>
-                <span>{site.tech.frontEnd}</span>
-              </p>
-              <p>
-                <b>Back End:</b>
-                <span>{site.tech.backEnd}</span>
-              </p>
-            </section>
-            <section>
-              {(site.url !== "")? (<h3>Link</h3>) : ""}
-              <p>
-                <a href={site.url} target="_blank">{site.url}</a>
-              </p>
-            </section>
-          </div>
-        </div>
-    )
-
-    return (
+const WebsitesFeature = () => (
       <section className="sites-section">
         <div className="gs-section-header" id="websites">
           <div className="gs-container">
@@ -47,12 +11,45 @@ const WebsitesFeature = (props) => {
           </div>
         </div>
         <div className="gs-container">
-          {renderSiteFeatures}
+          {Websites_Data.websites.map( (site, index) =>
+            <div className="site-container" key={index}>
+              <h2 className="sites-title">{site.title}</h2>
+              <div className="sites-images">
+                { site.images.map( (image, i) => (
+                  <img 
+                    src={image} 
+                    key={i}
+                    alt={`${site.title} screenshot ${i}`}
+                  />
+                ))}
+              </div>
+              <div className="sites-info">
+                <section>
+                  <h3>Description</h3>
+                  <p dangerouslySetInnerHTML={{ __html: site.desc }} />
+                </section>
+                <section>
+                  <h3>Technologies Used</h3>
+                  <p>
+                    <b>Front End:</b>
+                    <span>{site.tech.frontEnd}</span>
+                  </p>
+                  <p>
+                    <b>Back End:</b>
+                    <span>{site.tech.backEnd}</span>
+                  </p>
+                </section>
+                <section>
+                  {(site.url !== "")? (<h3>Link</h3>) : ""}
+                  <p>
+                    <a href={site.url} target="_blank">{site.url}</a>
+                  </p>
+                </section>
+              </div>
+            </div>
+          )}
         </div>
-
       </section>
-    )
-
-};
+)
 
 export default WebsitesFeature;
--- src/components/websites/websites_data.js
+++ src/components/websites/websites_data.js
@@ -1,10 +1,25 @@
 import mmHome from '../../images/mm-home.jpg'
-import mmSignup from '../../images/mm-signup.jpg'
+import mmMocks from '../../images/mm-mocks.png'
 import cirrooMusic from '../../images/cirroo-music.jpg'
 import cirrooHome from '../../images/cirroo-home.jpg'
+import soundscapeGenerator1 from '../../images/soundscape-generator1.jpg';
+import soundscapeGenerator2 from '../../images/soundscape-generator2.jpg';
 
 export const Websites_Data = {
   "websites": [
+    {
+      "title" : "Soundscape Generator",
+      "url": "https://github.com/amaclean199/soundscape-generator",
+      "desc" : `
+        Generate an audio backdrop for an image or video using image recognition and open source sounds. 
+        Hackathon project with <a href="http://www.alexmaclean.ca/" target="_blank">Alex MacLean</a>.
+      `,
+      "tech" : {
+        "frontEnd" : "React, Redux, Styled Components, Web Audio API, Google Resonance",
+        "backEnd" : "Python, AWS"
+      },
+      "images" : [soundscapeGenerator1, soundscapeGenerator2]
+    },
     {
       "title" : "MediaMade",
       "url": "",
@@ -13,7 +28,7 @@ export const Websites_Data = {
         "frontEnd" : "React, Redux, Webpack, PostCSS",
         "backEnd" : "Firebase"
       },
-      "images" : [mmHome, mmSignup]
+      "images" : [mmHome, mmMocks]
     },
     {
       "title" : "Cirroo",
